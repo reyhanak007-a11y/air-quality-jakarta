36ebeb565bcb740f4c57d470f584529b8435f00a
reyhanak <reyhanak007@gmail.com>
2025-11-21 15:31:53 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -221,4 +221,4 @@ def get_health_recommendations(category):
 # ==============================
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
